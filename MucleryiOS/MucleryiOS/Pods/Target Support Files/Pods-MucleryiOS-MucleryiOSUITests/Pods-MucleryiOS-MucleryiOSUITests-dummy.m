9a7e62552aead9f2f2eabf66712fcc8686127e0d
Blank <s180283@student.dhbw-mannheim.de>
2020-11-30 18:27:26 +0100

    delete useless

--- MucleryiOS/MucleryiOS/Pods/Target Support Files/Pods-MucleryiOS-MucleryiOSUITests/Pods-MucleryiOS-MucleryiOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MucleryiOS_MucleryiOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MucleryiOS_MucleryiOSUITests
-@end
--- MucleryiOS/MucleryiOS/Pods/Target Support Files/Pods-MucleryiOSTests/Pods-MucleryiOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MucleryiOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_MucleryiOSTests
-@end
